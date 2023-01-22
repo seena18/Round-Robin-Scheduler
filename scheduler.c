2bad5ad45924107a2c8ba6d3f2f472e7f12a4b6a
seena18 <seena101.sa@gmail.com>
2023-01-21 16:38:08 -0800

    freed memory

--- scheduler.c
+++ scheduler.c
@@ -24,15 +24,29 @@ int isNumber(char s[])
     return 1;
 }
 
+
 typedef struct process{
     char* name;
     int size;
     char* arguments[MAX_ARGUMENTS];
 }process;
-
+void printprocesses(process* p, int numprocesses)
+{
+    for (int i = 0; i<numprocesses; i++)
+    {
+        fprintf(stderr, "%s ",p[i].name);
+        for (int j = 0; j<p[i].size; j++){
+            fprintf(stderr, "%s ",p[i].arguments[j]);
+        }
+        fprintf(stderr, "\n");
+    }
+}
 int main(int argc, char *argv[])
 {
+    // array for keeping track of processes
     process* processes= (process *)malloc(sizeof(process));
+
+    // usage error checking
     if( argc < 3){
         fprintf(stderr, "Usage: schedule [milliseconds] [prog1 [prog1 args...]] [prog2 ...] ...\n");
         return 0;
@@ -44,23 +58,32 @@ int main(int argc, char *argv[])
 
     int index = 2;
     int numprocesses=0;
+    // parse and add all command line arguments to process array
     while(index<argc)
     {   
-        process* p = (process* )malloc(sizeof(process));
-        int size = 0 ;
-        p->name=(char*)malloc(sizeof(char)*strlen(argv[index]));
-        p->name=argv[index];
+    
+        process p;
+
+        int size = 0;
+        p.name=(char*)malloc(sizeof(char)*(strlen(argv[index])+1));
+        strcpy(p.name,argv[index]);
         numprocesses++;
         index++;
-        while(strcmp(argv[index],":")!=0 && index<argc){
-            p->arguments[size]=(char*)malloc(sizeof(char)*strlen(argv[index]));
-            p->arguments[size]=argv[index];
+        // delimiter for separating each process and their arguments
+        while(index<argc && strcmp(argv[index],":")!=0){
+            p.arguments[size]=(char*)malloc(sizeof(char)*(strlen(argv[index])+1));
+            strcpy(p.arguments[size],argv[index]);
             size++;
             index++;
         }
-        p->size=size;
+        
+        p.size=size;
         size=0;
-        process* reallocproccess= realloc(processes,numprocesses*sizeof(process));
+        // extend process array size upon appending process
+        processes[numprocesses-1]=p;
+        
+    
+        process* reallocproccess= realloc(processes,(numprocesses+1) * sizeof(process));
         if (reallocproccess) {
          processes = reallocproccess;
         }
@@ -68,7 +91,20 @@ int main(int argc, char *argv[])
             fprintf(stderr, "ERROR: allocating memory for process details");
 
         }
+        if(index<argc && strcmp(argv[index],":")==0){
+            index++;
+        }
    } 
+   printprocesses(processes,numprocesses);
+
+   for (int i = 0; i<numprocesses; i++)
+    {
+        free(processes[i].name);
+        for (int j = 0; j<processes[i].size; j++){
+            free(processes[i].arguments[j]);
+        }
+    }
+     free(processes);
 
 
 }
